b1bee390f512d9e7d04bdb16f53c51c4090fa2b1
Ernesto Garzon <danteunal@gmail.com>
2020-07-13 19:24:58 -0500

    fix/statusbar

--- src/app/product/components/product/product.component.ts
+++ src/app/product/components/product/product.component.ts
@@ -9,13 +9,27 @@ import {Product} from '@core/models/product';
 export class ProductComponent implements OnInit {
   @Input()
   product: Product = {};
-  
+  result: any;
+  percent:any;
+
+
   constructor() { }
   
   ngOnInit(): void {
+    this.barraestado();
+  }
+public barraestado(){
+
+  if (this.product.typeAccount === 'CREDIT_CARD'){
+
+    let currentbalance = this.product.productAccountBalances.saldo_actual.amount
+    let totallimit = this.product.productAccountBalances.cupo_total.amount
+    this.result =  totallimit - currentbalance
+    this.percent = currentbalance/totallimit*100 +'%'
     
-    console.log(this.product)
-    
+    console.log(this.percent);
+
   }
 
+}
 }
